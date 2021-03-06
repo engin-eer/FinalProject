eeba1cb86db7f75591a2f742160e3fa35d6a902c
engin-eer <engin.aydgdu@gmail.com>
2021-03-06 22:41:51 +0300

    6 Mart 2021

--- Core/Entities/Concrete/OperationClaim.cs
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -1,8 +1,9 @@
 ﻿namespace Core.Entities.Concrete
 {
-    public class OperationClaims:IEntity
+    public class OperationClaim:IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int OperationClaimId { get; set; }
     }
 }
